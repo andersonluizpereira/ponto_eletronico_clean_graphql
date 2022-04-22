6640f2dfc9463da481343336ac411a15d67e3e74
ANDERSON PEREIRA <andy2903.alp@gmail.com>
2022-04-22 13:08:12 -0300

    add protocols

---
+++ src/presentation/protocols/controller/controller.ts
@@ -0,0 +1,4 @@
+import { HttpResponse, HttpRequest } from '@/presentation/protocols'
+export interface Controller {
+  handle: (httpRequest: HttpRequest) => Promise<HttpResponse>
+}
--- src/presentation/protocols/index.ts
+++ src/presentation/protocols/index.ts
@@ -1,3 +1,4 @@
 export * from '@/presentation/protocols/validation/validation'
 export * from '@/presentation/protocols/http/http'
 export * from '@/presentation/protocols/middleware/middleware'
+export * from '@/presentation/protocols/controller/controller'
